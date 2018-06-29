95e52baf18be7ade02e870c4d1f48e197c85098c
Mustafa Morbel <mustafamorbel@Mustafas-MacBook-Pro.local>
2018-06-29 20:07:56 +0300

    404 page interval

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -29,7 +29,7 @@
 			<div class="cover-container h-100 row align-items-center">
 				<div role="main" class="inner cover col">
 					<h1 class="cover-heading">Page Not Found</h1>
-					<p class="lead">The requested page does not exist. We will try to automatically redirect you to our home page in {{seconds}} seconds.</p>
+					<p class="lead">The requested page does not exist.<hr class="clearfix">We will try to automatically redirect you to our home page in <span>{{seconds}}</span> seconds.</p>
 					<p class="lead">
 						<button class="btn btn-light" @click="$router.go(-1)">Go Back!</button>
 						<button class="btn btn-light" @click="$router.push({ name: 'home'})">Go To Home</button>
@@ -51,15 +51,18 @@
 		-ms-flex-pack: center;
 		-webkit-box-pack: center;
 		justify-content: center;
-		color: #fff;
+		color: white;
 		height: 100vh;
 		text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
 		box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
 	}
 	.cover-container {
-	  max-width: 42rem;
+	  max-width: 46rem;
 		  .cover {
 			padding: 0 1.5rem;
+			hr {
+				border-color: rgba(white, 0.25)
+			}
 			.btn {
 				font-weight: 700;
 				text-shadow: none
