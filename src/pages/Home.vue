5cab7cfd1471c2867f376fa404f95aadef308d2e
Mustafa Morbel <mustafamorbel@Mustafas-MacBook-Pro.local>
2018-06-29 19:22:31 +0300

    routing

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -27,8 +27,8 @@
 	          <h1 class="display-3">Hello, world!</h1>
 	          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
 	          <p>
-	          	<button class="btn btn-lg btn-outline-dark" @click="$router.push('404')" role="button">404 Page</button>
-	          	<button class="btn btn-lg btn-outline-dark" @click="$router.push({ name: 'samplePage'})">Go To Sample Page</button>
+	          	<button class="btn btn-outline-dark" @click="$router.push('404')" role="button">404 Page</button>
+	          	<button class="btn btn-outline-dark" @click="$router.push({ name: 'samplePage'})">Go To Sample Page</button>
 	          </p>
 	        </div>
 	      </div>
