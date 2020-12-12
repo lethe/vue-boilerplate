149d1e1e518f88ddad61cde3ac04f7cf0a89d173
Mustafa Morbel <mustafa.morbel@demirorenteknoloji.com>
2020-12-12 14:55:05 +0300

    upgraded to vue v3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="home">
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-3">Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p>
+          <button class="btn btn-outline-dark mr-2" @click="$router.push('404')" role="button">404 Page</button>
+          <button class="btn btn-outline-dark" @click="$router.push({ name: 'samplePage'})">Go To Sample Page</button>
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
---
+++ src/views/SamplePage.vue
@@ -0,0 +1,33 @@
+<script>
+  import MainHeader from "@/components/Header.vue";
+
+	export default {
+		name: 'SamplePage',
+		data() {
+			return {}
+		},
+		components: {
+      MainHeader,
+		},
+		computed: {
+		},
+		created() {
+
+		}
+	}
+</script>
+
+<template>
+	<section>
+    <main-header />
+		<div class="jumbotron">
+      <div class="container">
+        <h1 class="display-5">Hello from Sample Page</h1>
+        <p>
+          <button class="btn btn-outline-dark mr-2" @click="$router.go(-1)">Go Back!</button>
+          <button class="btn btn-outline-dark" @click="$router.push({ name: 'home'})">Go To Home</button>
+        </p>
+      </div>
+    </div>
+	</section>
+</template>
